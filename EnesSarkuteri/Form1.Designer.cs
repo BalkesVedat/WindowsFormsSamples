91aff22a95a78805cb067381f60dd63b25de7197
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-04-08 21:37:51 +0300

    Add files via upload

---
+++ EnesSarkuteri/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace EnesSarkuteri
+{
+    partial class frmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridCustomer = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.northwindDataSet = new EnesSarkuteri.NorthwindDataSet();
+            this.northwindDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.northwindCountryDataSet = new EnesSarkuteri.NorthwindCountryDataSet();
+            this.getCountryListTableAdapter = new EnesSarkuteri.NorthwindCountryDataSetTableAdapters.GetCountryListTableAdapter();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindCountryDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridCustomer
+            // 
+            this.dataGridCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCustomer.Location = new System.Drawing.Point(23, 209);
+            this.dataGridCustomer.Name = "dataGridCustomer";
+            this.dataGridCustomer.Size = new System.Drawing.Size(881, 241);
+            this.dataGridCustomer.TabIndex = 0;
+            this.dataGridCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnInsert.Location = new System.Drawing.Point(496, 47);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(87, 40);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Kayıt Ekle";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Location = new System.Drawing.Point(33, 22);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(65, 13);
+            this.lblCustomerID.TabIndex = 2;
+            this.lblCustomerID.Text = "Customer ID";
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(109, 20);
+            this.txtCustomerID.MaxLength = 5;
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(72, 20);
+            this.txtCustomerID.TabIndex = 3;
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(109, 47);
+            this.txtCompany.MaxLength = 40;
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(248, 20);
+            this.txtCompany.TabIndex = 5;
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Location = new System.Drawing.Point(33, 49);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(51, 13);
+            this.lblCompany.TabIndex = 4;
+            this.lblCompany.Text = "Company";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(182, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "*";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(357, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "*";
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(109, 73);
+            this.txtContact.MaxLength = 40;
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(248, 20);
+            this.txtContact.TabIndex = 9;
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Location = new System.Drawing.Point(33, 75);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(75, 13);
+            this.lblContact.TabIndex = 8;
+            this.lblContact.Text = "Contact Name";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(109, 99);
+            this.txtAddress.MaxLength = 40;
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(248, 42);
+            this.txtAddress.TabIndex = 11;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(33, 101);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(33, 149);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(43, 13);
+            this.lblCountry.TabIndex = 12;
+            this.lblCountry.Text = "Country";
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.DataSource = this.bindingSource1;
+            this.cmbCountry.DisplayMember = "Country";
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Location = new System.Drawing.Point(109, 148);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(248, 21);
+            this.cmbCountry.TabIndex = 13;
+            this.cmbCountry.ValueMember = "Country";
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // northwindDataSetBindingSource
+            // 
+            this.northwindDataSetBindingSource.DataSource = this.northwindDataSet;
+            this.northwindDataSetBindingSource.Position = 0;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataMember = "GetCountryList";
+            this.bindingSource1.DataSource = this.northwindCountryDataSet;
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged_1);
+            // 
+            // northwindCountryDataSet
+            // 
+            this.northwindCountryDataSet.DataSetName = "NorthwindCountryDataSet";
+            this.northwindCountryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getCountryListTableAdapter
+            // 
+            this.getCountryListTableAdapter.ClearBeforeFill = true;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(33, 176);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 14;
+            this.lblPhone.Text = "Phone";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(109, 176);
+            this.txtPhone.Mask = "(999) 000 00 00";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.PromptChar = '?';
+            this.txtPhone.Size = new System.Drawing.Size(248, 20);
+            this.txtPhone.TabIndex = 15;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Pink;
+            this.btnDelete.Location = new System.Drawing.Point(496, 156);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(87, 40);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnUpdate.Location = new System.Drawing.Point(496, 100);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(87, 40);
+            this.btnUpdate.TabIndex = 17;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // frmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.lblCompany);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.lblCustomerID);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridCustomer);
+            this.Name = "frmCustomer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Müşteri İşlemleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindCountryDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridCustomer;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label lblCustomerID;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.BindingSource northwindDataSetBindingSource;
+        private NorthwindDataSet northwindDataSet;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private NorthwindCountryDataSet northwindCountryDataSet;
+        private NorthwindCountryDataSetTableAdapters.GetCountryListTableAdapter getCountryListTableAdapter;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ EnesSarkuteri/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnesSarkuteri
+{
+    public partial class frmCustomer : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;User ID=sa;Password=1;");
+
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter adapter;
+
+        DataTable dt = new DataTable();
+
+        public frmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindCountryDataSet.GetCountryList' table. You can move, or remove it, as needed.
+            this.getCountryListTableAdapter.Fill(this.northwindCountryDataSet.GetCountryList);
+
+            //cmbCountry.SelectedIndex = -1;
+            cmbCountry.SelectedValue = "Türkiye";
+
+            MusteriListesiniYukle();
+
+        }
+
+        private void MusteriListesiniYukle()
+        {
+           // cmd.CommandText = "Select * FROM Customers";
+            cmd.CommandText = "SP_GetCustomerList";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            adapter = new SqlDataAdapter(cmd);
+
+
+            adapter.Fill(dt);
+
+            dataGridCustomer.DataSource = dt;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd.CommandText = "INSERT INTO Customers VALUES ('XYZ','XYZ Restoran','Ayşe Hanım', NULL, 'Bakırköy', 'İstanbul', NULL, NULL, 'Türkiye', '55555555', NULL)";
+
+                cmd.Connection = conn;
+
+                conn.Open();
+
+                int adet = cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                if (adet > 0)
+                {
+                    MessageBox.Show("Kayıt Eklendi.");
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Eklenemedi.");
+                }
+
+                MusteriListesiniYukle();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir sorun oluştu."+ ex.Message);
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
